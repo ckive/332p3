92448dfe91c2e9a4606f166fb020e7ea1533310d
Dan Yang <56417240+ckive@users.noreply.github.com>
2023-02-08 09:41:40 -0600

    structure code completed

---
+++ learningingames/environment.py
@@ -0,0 +1,42 @@
+from agent import OnlineLearner
+from typing import List
+import numpy as np
+from itertools import product
+
+class BiGameEnv:
+    def __init__(self, players: List[OnlineLearner], game):
+        self.players = players
+        self.game = game
+        gameN = game.shape[-1]
+        self.actionpaircounts = {ap: 0 for ap in product(range(gameN),range(gameN))}
+        
+    def run(self, N):
+        # runs the game for N rounds, players play simultaneously
+        # coordinates payoffs for agents in round i
+        J = len(self.players)
+        self.playeractions = np.zeros((J,N), dtype=int)
+        self.scoreboard = np.zeros((J,N))
+        
+        for i in range(N):
+            # perform action simultaneously for each agent
+            for j in range(J):
+                a_j = self.players[j].play()
+                self.playeractions[j][i] = a_j
+            # coordinate payoffs and update agents' weights
+            for j in range(J):
+                me = self.playeractions[j][i]
+                them = self.playeractions[j^1][i]
+                self.scoreboard[j][i] = self.game[me][them]
+                self.players[j].recv_feedback(me, self.scoreboard[j][i])
+
+        
+        return self.playeractions, self.scoreboard
+
+    def analyze_actions(self):
+        # go thru each of the action pairs and count occurence 
+        n = len(self.playeractions[0])
+        for i in range(n):
+            action_pair = (self.playeractions[0][i], self.playeractions[0][i])
+            self.actionpaircounts[action_pair] += 1
+        print(self.actionpaircounts)
+        return self.actionpaircounts
---
+++ learningingames/games.py
@@ -0,0 +1,39 @@
+import numpy as np
+import nashpy as nash
+class Bimatrix3x3:
+    """
+    Nx2x3x3 arrays where 1st dim is row(0) and col(1) player payoffs and for N rounds
+    """
+    def __init__(self):
+        # fixed
+        self.at_base = np.array(
+            [
+                [-6,-1,1],
+                [1,-6,-1],
+                [-1,1,-6]
+            ]
+        )
+        self.avoidties = np.array([self.at_base, self.at_base.transpose()])
+        self.atgame = nash.Game(self.at_base)
+        self.atg_xnash = self.atgame.support_enumeration()
+        
+        self.lt_base = np.array(
+            [
+                [-6,-1,1],
+                [1,-6,-1],
+                [-1,1,6]
+            ]
+        )
+        self.loveties = np.array([self.lt_base, self.lt_base.transpose()])
+        self.ltgame = nash.Game(self.lt_base)
+        self.ltg_xnash = self.ltgame.support_enumeration()
+
+        # can change
+        self.gen_mystery()
+
+    def gen_mystery(self):
+        self.mystery_base = np.random.randint(-10,10,(3,3))
+        self.mysterygame = np.array([self.mystery_base, self.mystery_base.transpose()])
+        mysgame = nash.Game(self.mystery_base)
+        mysg_xnash = mysgame.support_enumeration()
+    
